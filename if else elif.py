ab491343bf09934226761e427d5ac8aa83eba471
LincyXavier <lincyxavier2001@gmail.com>
2022-06-14 12:16:55 +0530

    voting

---
+++ if else elif.py
@@ -0,0 +1,7 @@
+num=int(input('enter a number'))
+if num<0:
+    print('entered number is negative')
+elif num==0:
+    print('entered number is zero')
+else:
+        print('entered number is positive')
